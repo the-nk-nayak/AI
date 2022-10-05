39c14e2caad6e267a5c3cd7d9828efa19af37348
the-nk-nayak <nishikantnayak2004@gmail.com>
2022-10-05 09:07:38 +0530

    My Jarvis😂😂

---
+++ Nk.py
@@ -0,0 +1,100 @@
+import pyttsx3 #pip install pyttsx3
+import speech_recognition as sr #pip install speechRecognition
+import datetime
+import webbrowser
+import os
+import smtplib
+
+engine = pyttsx3.init('sapi5')
+voices = engine.getProperty('voices')           # print(voices[1].id)
+engine.setProperty('voice', voices[0].id)
+
+
+def speak(audio):
+    engine.say(audio)
+    engine.runAndWait()
+
+
+def wishMe():
+    hour = int(datetime.datetime.now().hour)
+    if hour>=0 and hour<12:
+        speak("Good Morning!")
+
+    elif hour>=12 and hour<18:
+        speak("Good Afternoon!")
+
+    else:
+        speak("GOOD Evening!")
+
+    speak("I am Jarvis Sir.  I am being developed by Nk Nayak.  Please tell me how may I help you")
+
+def takeCommand():
+
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Listening...")
+        r.pause_threshold = 1
+        audio = r.listen(source)
+
+    try:
+        print("Recognizing...")
+        query = r.recognize_google(audio, language='en-in')
+        print(f"User said: {query}\n")
+
+    except Exception as e:
+        # print(e)
+        print("Say that again please...")
+        return "None"
+    return query
+
+if __name__ == "__main__":
+    wishMe()
+    while True:
+    # if 1:
+        query = takeCommand().lower()
+
+        if 'open youtube' in query:
+            webbrowser.open("youtube.com")
+
+        elif 'open google' in query:
+            webbrowser.open("google.com")
+
+        elif 'open stackoverflow' in query:
+            webbrowser.open("stackoverflow.com")
+        
+        elif 'open facebook' in query:
+            webbrowser.open("facebook.com")
+
+        elif 'open Instagram' in query:
+            webbrowser.open("instagram.com")
+
+        elif 'open whatsapp' in query:
+            webbrowser.open("web.whatsapp.com")
+        
+
+        elif 'play music' in query:
+            music_dir = 'D:\MyPlayList'
+            songs = os.listdir(music_dir)
+            print(songs)
+            os.startfile(os.path.join(music_dir, songs[0]))
+
+        elif 'the time' in query:
+            strTime = datetime.datetime.now().strftime("%H:%M:%S")
+            speak(f"Sir, the time is {strTime}")
+
+        elif 'open code' in query:
+            codePath = "C:\\Users\\chenuli\\AppData\\Local\\Programs\\Microsoft VS Code\\levelmenu.exe"
+            os.startfile(codePath)
+
+        elif 'email to Victoria' in query:
+            try:
+                speak("What should I say?")
+                content = takeCommand()
+                to = "chenuliilz@gmail.com.com"
+                sendEmail(to, content)
+                speak("Email has been sent!")
+            except Exception as e:
+                print(e)
+                speak("Sorry my friend Unity buddy. I am not able to send this email")
+        elif 'break' in query:
+            break
